38a60bde4c4363ab4b8bec2fb498ef8ea9a3842f
Quetuo <quetuo@quetuo.net>
2011-10-31 18:01:00 +0000

    Update core/db.php

--- core/db.php
+++ core/db.php
@@ -3,8 +3,8 @@
 	
 	$config ['db'] ['host'] = 'localhost';
 	$config ['db'] ['user'] = 'root';
-	$config ['db'] ['pass'] = 'jammi6369';
-	$config ['db'] ['db'] = 'quetuo.net';
+	$config ['db'] ['pass'] = 'password';
+	$config ['db'] ['db'] = 'database';
 	
 	$db = new mysqli ($config ['db'] ['host'], $config ['db'] ['user'], $config ['db'] ['pass'], $config ['db'] ['db']);
 ?>
